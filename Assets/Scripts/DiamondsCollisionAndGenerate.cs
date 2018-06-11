58df632a9ee5968f81f770141a36ff02459c9563
szotowicz <mikolaj.szotowicz@gmail.com>
2018-06-11 23:25:03 +0200

    auto generate of obstacles and background objects

--- Assets/Scripts/DiamondsCollisionAndGenerate.cs
+++ Assets/Scripts/DiamondsCollisionAndGenerate.cs
@@ -10,15 +10,20 @@ public class DiamondsCollisionAndGenerate : MonoBehaviour
 {
     public Text PointsMonitor;
     public Text BestScoreMonitor;
-    public int DiamondsOnStart = 40;
+    public int DiamondsOnStart = 15;
     public List<GameObject> DiamondsPrefabs;
+    public int ObstaclesOnStart = 15;
+    public List<GameObject> ObstaclePrefabs;
+    public List<GameObject> BackgroundObjPrefabs;
 
     private string nameOfDiamond = "Diamond";
     private string nameOfObstacle = "ToDie";
+    private string nameOfBackgroundObj = "BackgroundObj";
     private string bestScoreFileName = "/bestScore.txt";
     private int points;
-    private float lastDiamondPossition = 0;
-    private List<GameObject> createdDiamonds = new List<GameObject>();
+    private float lastCreatedObjectPosstion = 0;
+    private float lastCreatedBackgroundObjectPosstion = 0;
+    private List<GameObject> createdObject = new List<GameObject>();
     private Dictionary<string, float> possiblePaths = new Dictionary<string, float>()
     {
         {"left", -2.0f },
@@ -35,16 +40,33 @@ public class DiamondsCollisionAndGenerate : MonoBehaviour
         {
             CreateNewBestScoreFile();
         }
-        
+
         ShowBestScore();
-        
-        if (DiamondsPrefabs.Count > 0)
+        while (DiamondsOnStart > 0 && ObstaclesOnStart > 0)
         {
-            // Debug.Log("Diamonds: " + DiamondsPrefabs.Count);
-            for (int i = 0; i < DiamondsOnStart; i++)
+            int objType = Random.Range(0, 2);
+
+            if (objType == 0)
             {
-                AddNewDiamond();
+                if (ObstaclePrefabs.Count > 0)
+                {
+                    CreateNewObject(ObstaclePrefabs);
+                    ObstaclesOnStart--;
+                }
             }
+            else if (objType == 1)
+            {
+                if (DiamondsPrefabs.Count > 0)
+                {
+                    CreateNewObject(DiamondsPrefabs);
+                    DiamondsOnStart--;
+                }
+            }
+        }
+
+        for (int i = 0; i < 15; i++)
+        {
+            CreateNewBackgroundObj();
         }
     }
 
@@ -53,23 +75,23 @@ public class DiamondsCollisionAndGenerate : MonoBehaviour
         float gameCharacterDistance = gameObject.transform.position.z;
         // Debug.Log (gameCharacterDistance);
 
-        foreach (GameObject diamond in createdDiamonds)
+        foreach (GameObject obj in createdObject)
         {
-            if (gameCharacterDistance > diamond.transform.position.z + 3.0f)
+            if (gameCharacterDistance > obj.transform.position.z + 10.0f)
             {
-                RemoveDiamond(diamond);
+                RemoveCreatedObject(obj);
                 break;
             }
         }
     }
-    
+
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.name.Contains(nameOfDiamond))
         {
             points++;
             ShowPoints();
-            RemoveDiamond(col.gameObject);
+            RemoveCreatedObject(col.gameObject);
         }
         else if (col.gameObject.name.Contains(nameOfObstacle))
         {
@@ -95,50 +117,85 @@ public class DiamondsCollisionAndGenerate : MonoBehaviour
             BestScoreMonitor.text = bestScore;
         }
     }
-
-    private void AddNewDiamond()
+    
+    private void CreateNewObject(List<GameObject> prefabsList)
     {
-        int newModelIndex = Random.Range(0, DiamondsPrefabs.Count);
-        // Debug.Log(newModelIndex);
+        int newModelIndex = Random.Range(0, prefabsList.Count);
         float newModelX = possiblePaths.ElementAt(Random.Range(0, possiblePaths.Count)).Value;
-        float distanceForNewModel = Random.Range(2.0f, 10.0f);
-        lastDiamondPossition += distanceForNewModel;
+        float distanceForNewModel = Random.Range(5.0f, 15.0f);
+        lastCreatedObjectPosstion += distanceForNewModel;
 
-        GameObject newDiamond = Instantiate(DiamondsPrefabs[newModelIndex],
-            new Vector3(newModelX, -3.0f, lastDiamondPossition), Quaternion.identity);
+        GameObject newObjectModel = Instantiate(prefabsList[newModelIndex],
+            new Vector3(newModelX, -1.0f, lastCreatedObjectPosstion), Quaternion.identity);
 
-        createdDiamonds.Add(newDiamond);
+        createdObject.Add(newObjectModel);
     }
 
-    private void RemoveDiamond(GameObject diamond)
+    private void CreateNewBackgroundObj()
     {
-        createdDiamonds.Remove(diamond);
-        Destroy(diamond);
+        int newModelIndex = Random.Range(0, BackgroundObjPrefabs.Count);
 
-        AddNewDiamond();
+        GameObject newObjectModel = Instantiate(BackgroundObjPrefabs[newModelIndex],
+            new Vector3(3.5f + Random.Range(0.0f, 2.0f), 0.0f, lastCreatedBackgroundObjectPosstion + Random.Range(2.0f, 5.0f)), Quaternion.identity);
+        createdObject.Add(newObjectModel);
+
+        newObjectModel = Instantiate(BackgroundObjPrefabs[newModelIndex],
+            new Vector3(-3.5f - Random.Range(0.0f, 2.0f), 0.0f, lastCreatedBackgroundObjectPosstion + Random.Range(2.0f, 5.0f)), Quaternion.identity);
+        createdObject.Add(newObjectModel);
+
+        newObjectModel = Instantiate(BackgroundObjPrefabs[newModelIndex],
+            new Vector3(6.0f + Random.Range(0.0f, 2.0f), 0.0f, lastCreatedBackgroundObjectPosstion + Random.Range(1.5f, 5.0f)), Quaternion.identity);
+        createdObject.Add(newObjectModel);
+
+        newObjectModel = Instantiate(BackgroundObjPrefabs[newModelIndex],
+            new Vector3(-6.0f - Random.Range(0.0f, 2.0f), 0.0f, lastCreatedBackgroundObjectPosstion + Random.Range(1.5f, 5.0f)), Quaternion.identity);
+        createdObject.Add(newObjectModel);
+
+        lastCreatedBackgroundObjectPosstion += 5.0f;
+    }
+
+    private void RemoveCreatedObject(GameObject objToRemove)
+    {
+        createdObject.Remove(objToRemove);
+        Destroy(objToRemove);
+
+        if (objToRemove.name.Contains(nameOfDiamond))
+        {
+            CreateNewObject(DiamondsPrefabs);
+        }
+        else if (objToRemove.name.Contains(nameOfObstacle))
+        {
+            CreateNewObject(ObstaclePrefabs);
+        }
+        else if (objToRemove.name.Contains(nameOfBackgroundObj))
+        {
+            Debug.Log("elo");
+            CreateNewBackgroundObj();
+        }
+        Debug.Log(objToRemove.name);
     }
 
     private void GameOver()
     {
         Debug.Log("You are dead");
-        
+
         // Update best score
         if (File.Exists(Application.persistentDataPath + bestScoreFileName))
         {
             string currentBestScore = File.ReadAllText(Application.persistentDataPath + bestScoreFileName);
-            
+
             if (int.Parse(currentBestScore) < points)
             {
-                // Show message about record
                 File.WriteAllText(Application.persistentDataPath + bestScoreFileName, points.ToString());
+                // TODO: Show message about record
             }
             else
             {
-                // Show message
+                // TODO: Show message
             }
         }
 
-        Debug.Log("return to menu");
+        //Debug.Log("return to menu");
         SceneManager.LoadScene("MainMenu");
     }
 }
